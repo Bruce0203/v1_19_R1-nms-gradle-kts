1be3ea613210f572b9daa1fb2749fb3ee1fa648a
Bruce0203 <BruceFreedy@gmail.com>
2022-09-25 18:14:32 +0900

    chore: build dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,10 +32,20 @@ application {
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://repo.dmulloy2.net/repository/public/") }
+    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
+    maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") }
+    maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/public/") }
+    maven { url = uri("https://jitpack.io") }
+    maven { url = uri("https://libraries.minecraft.net/") }
+    maven { url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/") }
 }
 
 dependencies {
     api(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     api("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+    compileOnly("org.spigotmc:spigot-api:1.19.2-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot:1.19.2-R0.1-SNAPSHOT")
+    compileOnly("com.mojang:authlib:1.5.21")
 }
 
